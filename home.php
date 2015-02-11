c496c0f0cf7d24b1cd4de929a47fff5a6c672e12
Thiago Brito de Lima <thitheguy@gmail.com>
2015-02-11 15:56:10 -0200

    Home ajusts
    Some details were cleared from the head.

--- home.php
+++ home.php
@@ -1,7 +1,6 @@
 <?php
 require 'core/init.php';
 $general->logged_out_protect();
-
 $username 	= htmlentities($user['username']); // storing the user's username after clearning for any html tags.
 
 ?>
