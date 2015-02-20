a721671f3f43f06069c4d6c2c832cdf69538e38e
Thiago Brito de Lima <thitheguy@gmail.com>
2015-02-20 16:32:49 -0200

    Change Password
    Just ajusted !DOCTYPE statment.

--- change-password.php
+++ change-password.php
@@ -3,7 +3,6 @@ include_once 'core/init.php';
 $general->logged_out_protect();
 ?>
 
-<!doctype html>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
