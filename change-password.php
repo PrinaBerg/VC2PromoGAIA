c30d926f89739e82d2f8dcaf9a37772256e97333
Thiago Brito de Lima <thitheguy@gmail.com>
2015-02-24 11:42:33 -0300

    Change Password translate
    <?= alterar senha ?>

--- change-password.php
+++ change-password.php
@@ -86,7 +86,7 @@ $general->logged_out_protect();
                         <label>Confirmar:</label>
                         <input type="password" name="password_again"><br/><br/>
 
-                        <input type="submit" value="Mudar Senha">
+                        <input type="submit" value="Alterar Senha">
 
                       <?php  if (isset($_GET['success']) === true && empty ($_GET['success']) === true ) {
                         echo '<h4 id="approved">Senha alterada com sucesso!</h4>';
